405efcc89d32ca7a7e15a20e79a84d9aba9a68fc
kevinchenyuanji-maker <kevinchenyuanji@gmail.com>
2025-09-16 23:24:02 +0800

    Add files via upload

---
+++ mvd_pwa/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open('mvd-v1').then(c => c.addAll(['./','./index.html','./style.css','./main.js','./manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', evt => { self.clients.claim(); });
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
+});
